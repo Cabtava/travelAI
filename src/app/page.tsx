7bbb50f2c774137a0c94f10ece18e111f9c57c47
gigi <gigidi103ze@gmail.com>
2025-06-01 17:13:18 +0400

    03: authentication setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Error creating user: ");
+        },
+        onSuccess: () => {
+          window.alert("User created successfully!");
+        },
+      }
+    );
+  };
+
   return (
-    <div>
-      <Button>Click Me!</Button>
+    <div className="p-4 flex flex-col gap-4">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Create User</Button>
     </div>
   );
 }
